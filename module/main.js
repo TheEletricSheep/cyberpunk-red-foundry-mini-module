81d9a4d5400aeda0e402c1492e969fd70000cd54
TheEletricSheep <ramuthesheep@gmail.com>
2026-06-30 00:19:41 -0300

    Update main.js

--- module/main.js
+++ module/main.js
@@ -2,8 +2,10 @@ import "./powerRebuild.js";
 import "./Autosear.js";
 import "./newRed.js";
 import "./critWeapons.js ";
+import "./Aimedextradamage.js";
 import "./fire.js";
 import "./smartAmmo.js";
 import "./charge.js";
 import "./Autofireshotgun.js";
 console.log("Cyberpunk Red Mini Module Loaded");
+
